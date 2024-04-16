135fe1201c32d67c95a851c6a5bce70b80c56263
edumoreira7 <161092739+edumoreira7@users.noreply.github.com>
2024-04-16 23:27:29 +0000

    organização de funções e criação .h e .c

---
+++ contatos.c
@@ -0,0 +1,23 @@
+#include "contatos.h"
+#include <stdio.h>
+
+int adicionarC(Contato contatos[], int *pos){
+  printf("funcao de criar contato\n");
+  return 0;
+}
+int deletarC(Contato contatos[], int *pos){
+  printf("funcao de deletar contato\n");
+  return 0;
+}
+int listarC(Contato contatos[], int pos){
+  printf("funcao de listar contato\n");
+  return 0;
+}
+int salvarC(Contato contatos[], int total, int pos){
+  printf("funcao de salvar contato\n");
+  return 0;
+}
+int carregarC(Contato contatos[], int total, int pos){
+  printf("funcao de carregar contato\n");
+  return 0;
+}
