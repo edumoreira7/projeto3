bb3cbc513eaafb5da35163fbc0831ad7e1b5c491
edumoreira7 <161092739+edumoreira7@users.noreply.github.com>
2024-04-19 05:49:44 +0000

    Imcrementando função criar e listar

--- contatos.c
+++ contatos.c
@@ -1,8 +1,29 @@
 #include "contatos.h"
 #include <stdio.h>
+#include <string.h>
 
 int adicionarC(Contato contatos[], int *pos){
-  printf("funcao de criar contato\n");
+  if (*pos >= TOTAL)
+    return 1;
+
+  printf("Digite o telefone: ");
+  scanf("%lld", &contatos[*pos].telefone);
+  clearBuffer();
+  
+  printf("Digite o nome: ");
+  fgets(contatos[*pos].nome, 400, stdin);
+  contatos[*pos].nome[strcspn(contatos[*pos].nome, "\n")] = '\0';
+  
+  printf("Digite o sobrenome: ");
+  fgets(contatos[*pos].sobrenome, 400, stdin);
+  contatos[*pos].sobrenome[strcspn(contatos[*pos].sobrenome, "\n")] = '\0';
+
+  printf("Digite o email: ");
+  fgets(contatos[*pos].email, 400, stdin);
+  contatos[*pos].email[strcspn(contatos[*pos].email, "\n")] = '\0';
+
+  *pos = *pos +1;
+
   return 0;
 }
 int deletarC(Contato contatos[], int *pos){
@@ -10,7 +31,17 @@ int deletarC(Contato contatos[], int *pos){
   return 0;
 }
 int listarC(Contato contatos[], int pos){
-  printf("funcao de listar contato\n");
+  if(pos == 0)
+    return 1;
+
+  for(int i=0; i<pos; i++){
+    printf("Nome: %s ", contatos[i].nome);
+    printf("%s\n", contatos[i].sobrenome);
+    printf("Email: %s\n", contatos[i].email);
+    printf("Telefone: %lld\n", contatos[i].telefone);
+    printf("------------------------\n");
+  }
+
   return 0;
 }
 int salvarC(Contato contatos[], int total, int pos){
@@ -20,4 +51,8 @@ int salvarC(Contato contatos[], int total, int pos){
 int carregarC(Contato contatos[], int total, int pos){
   printf("funcao de carregar contato\n");
   return 0;
+}
+void clearBuffer(){
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) { }
 }
